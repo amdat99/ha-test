7d10769cf638d339543cc8abd8387ff8e955bea5
Aamir Adat <74106642+amdat99@users.noreply.github.com>
2022-02-10 15:02:28 +0000

    change title color and add toggle to show more button

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // TODO - I haven't quite tested this properly
-$(document).ready(function() {
-	$( "#show-hidden-section" ).click(function(event) {
-		event.preventDefault();
-		$('#hidden-section').slideDoown();
-	});
+$(document).ready(function () {
+  let hidden = true;
+  $("#show-hidden-section").click(function (event) {
+    event.preventDefault();
+    if (hidden) {
+      $("#show-hidden-section").text("Show less");
+      hidden = false;
+    } else {
+      $("#show-hidden-section").text("Show more");
+      hidden = true;
+    }
+    $("#hidden-section").slideToggle();
+  });
 });
